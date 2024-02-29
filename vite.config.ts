5a4c57fd43d58a42a292e4d5424274c8c2bba960
r-miyashita <rmiyashita338@gmail.com>
2024-02-29 23:19:30 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-ver2/',
   plugins: [
-    vue(),
+    vue({
+        // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+        // For example <img src="/images/logo.png"> will not work without the code below
+        template: {
+            transformAssetUrls: {
+                includeAbsolute: false,
+            },
+        },
+    }),
   ],
   resolve: {
     alias: {
